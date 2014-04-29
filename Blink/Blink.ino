25a50cced361d4c62c42c6e2ca0be4227a6ab647
skeimeier <stefan-keimeier@t-online.de>
2014-04-29 15:40:08 +0200

    angepasst auf JeeMicro BoardLed

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -4,17 +4,17 @@
  
   This example code is in the public domain.
  */
-
+#define LED 9
 void setup() {                
   // initialize the digital pin as an output.
   // Pin 13 has an LED connected on most Arduino boards:
-  pinMode(4, OUTPUT);     
+  pinMode(LED, OUTPUT);     
 }
 
 void loop() {
-  digitalWrite(4, HIGH);   // set the LED on
+  digitalWrite(LED, HIGH);   // set the LED on
   delay(2000);              // wait for a second
-  digitalWrite(4, LOW);    // set the LED off
-  delay(2000);              // wait for a second
+  digitalWrite(LED, LOW);    // set the LED off
+  delay(100);              // wait for a second
 }
 
